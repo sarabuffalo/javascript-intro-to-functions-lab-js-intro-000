2d1164f1fb05906814b98d16aa85979682d00ade
sarabuffalo <sep.14094@yahoo.com>
2018-04-25 23:17:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 function shout(string) {
   return string.toUpperCase()
 }
+
+function whisper(HELLO) {
+  return HELLO.toLowerCase()
+}
+
+
+function logShout(hello) {
+  console.log('hello').toUpperCase()
+}
+
+function logWhisper(HELLO) {
+  console.log(HELLO).toLowerCase()
+}
+
+function sayHiToGrandma(string) {
+  if "hello" {
+    return "I can/'t hear you!" {
+  else "HELLO" {
+    return "YES INDEED!" {
+  else if "I love you, Grandma." {
+    return "I love you, too."
+  }
+}
