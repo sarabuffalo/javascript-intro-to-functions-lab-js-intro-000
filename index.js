8a7156ff4030b547274aac571a756aff04901c9c
sarabuffalo <sep.14094@yahoo.com>
2018-04-25 23:50:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ function logWhisper(HELLO) {
   console.log(HELLO).toLowerCase()
 }
 
-function sayHiToGrandma(string) {
-  if (hello) {
-    return "I can/'t hear you!"
-  } else if (HELLO) {
-    return "YES INDEED!"
-  } else if (I love you, Grandma.) {
-    return "I love you, too."
-  }
-}
+// function sayHiToGrandma(string) {
+//   if (hello) {
+//     return "I can/'t hear you!"
+//   } else if (HELLO) {
+//     return "YES INDEED!"
+//   } else if (I love you, Grandma.) {
+//     return "I love you, too."
+//   }
+// }
